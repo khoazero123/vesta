84727546678751c90c1f1ab15a80e596ee0840c3
Võ Văn Khoa <khoazero123@gmail.com>
2017-08-24 13:54:19 +0700

    File manager start location to home folder for admin

--- web/list/directory/index.php
+++ web/list/directory/index.php
@@ -5,8 +5,7 @@ include($_SERVER['DOCUMENT_ROOT'] . "/inc/main.php");
 
 
 if ((!isset($_SESSION['FILEMANAGER_KEY'])) || (empty($_SESSION['FILEMANAGER_KEY']))) {
-    header("Location: /filemanager-not-purchased/");
-    exit;
+    // header("Location: /filemanager-not-purchased/");exit;
 }
 
 // Check login_as feature
@@ -24,11 +23,16 @@ if (empty($panel)) {
     $panel = json_decode(implode('', $output), true);
 }
 
+$ROOT_DIR = $user == 'admin' ? '' : $panel[$user]['HOME'];
+
 $path_a = !empty($_REQUEST['dir_a']) ? $_REQUEST['dir_a'] : '';
 $path_b = !empty($_REQUEST['dir_b']) ? $_REQUEST['dir_b'] : '';
+
+if(empty($ROOT_DIR) && empty($path_a) && empty($path_b)) $path_a = $path_b = $panel[$user]['HOME'];
+
 $GLOBAL_JS  = '<script type="text/javascript">GLOBAL.START_DIR_A = "' . $path_a . '";</script>';
 $GLOBAL_JS .= '<script type="text/javascript">GLOBAL.START_DIR_B = "' . $path_b . '";</script>';
-$GLOBAL_JS .= '<script type="text/javascript">GLOBAL.ROOT_DIR = "' . $panel[$user]['HOME'] . '";</script>';
+$GLOBAL_JS .= '<script type="text/javascript">GLOBAL.ROOT_DIR = "' . $ROOT_DIR . '";</script>';
 
 
 // Footer
